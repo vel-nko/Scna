3ed5c838c0c1c3fa179ef4030bdc2930ca5c6e9a
machinecore <a.bivzyuk@gmail.com>
2019-10-27 13:29:00 +0200

    add gallery

--- app/common.js
+++ app/common.js
@@ -3,11 +3,26 @@
 import './sass/main.sass';
 let script = require('scriptjs');
 script('https://unpkg.com/swiper/js/swiper.min.js', function (){
-  let mySwiper = new Swiper ('.review-gallery', {
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
     slideActiveClass: 'active',
     navigation: {
-      prevEl: '.slider-btn.prev',
-      nextEl: '.slider-btn.next',
+      nextEl: '.review-section .btn.next',
+      prevEl: '.review-section .btn.prev',
+      disabledClass: 'disabled'
+    },
+    thumbs: {
+      swiper: galleryTop
+    }
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.review-section .btn.next',
+      prevEl: '.review-section .btn.prev',
+      disabledClass: 'disabled'
     },
   });
 
