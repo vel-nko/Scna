ed26b64e4ec0dcab0be97280a86692b7a0699dcf
machinecore <a.bivzyuk@gmail.com>
2019-10-27 12:32:54 +0200

    add styles

--- app/common.js
+++ app/common.js
@@ -1,3 +1,14 @@
 'use strict';
 
 import './sass/main.sass';
+let script = require('scriptjs');
+script('https://unpkg.com/swiper/js/swiper.min.js', function (){
+  let mySwiper = new Swiper ('.review-gallery', {
+    slideActiveClass: 'active',
+    navigation: {
+      prevEl: '.slider-btn.prev',
+      nextEl: '.slider-btn.next',
+    },
+  });
+
+})
